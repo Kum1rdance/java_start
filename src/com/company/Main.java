811c6b6b7c9b78de4ab8094c6f089819ca217b9e
Kum1rdance <nazar200311111@gmail.com>
2021-09-13 22:25:10 +0300

    wrote scode to shiffer...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("java");
+        char[] alph = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        String toMask = "CDE"; //ABC
+        String masked = "";
+        for (int i =0; i< toMask.length(); i++)
+        {
+            char letter = toMask.charAt(i);
+            int idx = findIndex(alph,letter);
+            masked += alph[idx-2<0 ? alph.length - idx-2:idx-2];
+
+        }
+        System.out.println(toMask);
+        System.out.println(masked);
+    }
+
+    public static int findIndex(char[] arr, char target){
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
